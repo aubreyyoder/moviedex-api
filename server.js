4aed13b3a84e597562052d692160fe91920f9666
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-07-22 16:47:11 -0700

    Completed assignment

--- server.js
+++ server.js
@@ -1,39 +1,51 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-
-console.log(process.env.API_TOKEN);
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIEDEX = require("./moviedex.json");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
 
-const validGenres = [
-  "Animation",
-  "Drama",
-  "Romantic",
-  "Comedy",
-  "Spy",
-  "Crime",
-  "Thriller",
-  "Adventure",
-  "Documentary",
-  "Horror",
-  "Action",
-  "Western",
-  "History",
-  "Biography",
-  "Musical",
-  "Fantasy",
-  "War",
-  "Grotesque"
-];
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
 
-function handleGetGenres(req, res) {
-  res.json(validGenres);
-}
+  console.log("validate bearer token middleware");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
 
-app.get("/genres", handleGetGenres);
+  next();
+});
+
+app.get("/movie", function handleGetMovie(req, res) {
+  let response = MOVIEDEX;
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+});
 
 const PORT = 8000;
 
