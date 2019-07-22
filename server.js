2a99c3b7c4c636683ea60ff84571f9404f2cadb9
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-07-22 15:41:46 -0700

    added genres endpoint

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+
+console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello, world!");
-});
+const validGenres = [
+  "Animation",
+  "Drama",
+  "Romantic",
+  "Comedy",
+  "Spy",
+  "Crime",
+  "Thriller",
+  "Adventure",
+  "Documentary",
+  "Horror",
+  "Action",
+  "Western",
+  "History",
+  "Biography",
+  "Musical",
+  "Fantasy",
+  "War",
+  "Grotesque"
+];
+
+function handleGetGenres(req, res) {
+  res.json(validGenres);
+}
+
+app.get("/genres", handleGetGenres);
 
 const PORT = 8000;
 
